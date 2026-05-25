254b93c783841186f813518acf040037c0bb0803
ymface <ymface4575@gmail.com>
2026-05-25 23:32:21 +0900

    improved foot step

---
+++ Source/Soulslike/Public/ANS/SLAN_PlayRandomSound.h
@@ -0,0 +1,29 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+
+#include "SLAN_PlayRandomSound.generated.h"
+
+
+UCLASS()
+class SOULSLIKE_API USLAN_PlayRandomSound : public UAnimNotify
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, Category = "SoundLocation")
+    FName socket_for_sound;
+    UPROPERTY(EditAnywhere, Category = "Sound")
+    TObjectPtr<USoundBase> Sound;
+
+    virtual void Notify(
+        USkeletalMeshComponent* MeshComp,
+        UAnimSequenceBase* Animation,
+        const FAnimNotifyEventReference& EventReference
+    ) override;
+
+};
+};
